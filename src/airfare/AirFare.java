b2a902102a68ceb53af3d4036ce1c26e8b5aa4de
JeremyTheGreats <rabanesjeremy@gmail.com>
2025-08-12 22:43:07 -0700

    AirFair

---
+++ src/airfare/AirFare.java
@@ -0,0 +1,49 @@
+
+package airfare;
+
+import java.util.Scanner;
+
+public class AirFare {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        float result = 0;
+        
+        System.out.print("Distance ( km ) : ");
+        int distance = input.nextInt();
+        
+        System.out.print("Class (1 or 2 ) : ");
+        int cass = input.nextInt();
+        
+            if( cass == 1 ){
+                
+                result = 250 * distance;
+                    
+                    if( distance >= 1000 ){
+                        result = result * ( float ) 0.1;
+                    }
+                System.out.printf("Total fare : %.2f\n", result );
+
+            }
+            else if( cass == 2 ){
+                
+                result = 500 * distance;
+                    
+                    if( distance >= 1000 ){
+                            result = result * ( float ) 0.1;
+                        }
+                System.out.printf("Total fare : %.2f\n", result );
+            }
+            else{
+                System.out.println("Invalid Class! Try again!");
+            }
+            
+        
+            
+            
+        
+    }
+    
+}
